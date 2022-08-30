dc86867f7d9b63d79d861905b19ee66dadfd15e2
kenshin000 <keta.0829.a@gmail.com>
2022-08-30 21:46:58 +0900

    ログインページビュー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
+
+      has_many :languages
+      has_many :comments
+      has_many :posts
 end
