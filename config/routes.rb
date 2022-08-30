ca029d0985528ccbbf44773a8887a41bf75b97e0
kenshin000 <keta.0829.a@gmail.com>
2022-08-30 16:26:37 +0900

    languageテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :users do
-    get '/users', to: redirect("/users/sign_up")
-  end
+  root to: 'languages#index'
+  resources :languages, only: :index
 end
