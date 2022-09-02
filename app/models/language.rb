3797c4ac7e95b3e069d4ad59ba54c8c2bc2dc0a9
kenshin000 <keta.0829.a@gmail.com>
2022-09-02 17:03:17 +0900

    言語のテーブルへの保存機能

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,9 @@
 class Language < ApplicationRecord
 
+  validates :name, presence: true
+  validates :user_id, presence: true
 
+  belongs_to :user
+  has_many :posts
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'languages#index'
-  resources :languages, only: :index
+  resources :languages, only: [:index, :new, :create]
 end
